a1925a707bfcde0271af98cc85cfe9744b67e0ca
GenJune <julia.nepochatova@gmail.com>
2025-10-20 21:05:55 +0300

    4 коммит

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 
 #include <iostream>
 int**creak(size_t rows,size_t cols);
-void remove(int* m,size_t rows, sixe_t cols);
-//я не успеваю писать :(
+void remove(int* m,size_t rows, sixe_t cols){
+   m[i],0<=i<rows;
+   for (size_t i=0; i<rows;++i){
+      delete[]m[i];
+   }
+   delete [] m;
+}
 void input(int**m, size_t rows, size_t cols){
    for (size_t i=0, i<rows&&(std::cin),++i){
       for (size_t j=0, j<cold&&(std::cin,++j){
-           std::cin>>[i][j];
+           std::cin>>m[i][j];
 
 void output(const int*const*m, size_t rows, size_t cols){
-   std::cout<<rows<<" "<<cols
+   std::cout<<rows<<" "<<cols;
 
 int main(){
    size_t rows =0, cols = 0;
@@ -25,6 +30,7 @@ int main(){
    }
    output(m,rows,cols);
    remove(m,rows,cols);
+   std::cout<<"\n";
 }
 int**create(size_t r,size_t c){
    int**result = new int*[r];
